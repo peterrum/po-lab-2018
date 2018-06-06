caef1f2c7e9145b8e6196f51595f67acd5ee3e0f
alex <Pan1cfr3ny>
2018-06-06 18:37:22 +0200

    added State class and necessary visitor methods

--- src/vsa_visitor.cpp
+++ src/vsa_visitor.cpp
@@ -4,8 +4,24 @@ using namespace llvm;
 
 namespace pcpo {
 
-void VsaVisitor::visitInstruction(Instruction &I){
-    errs() << I.getOpcodeName() << ": " << I.getValueID() << "\n";
+
+bool State::put(Value& v, std::unique_ptr<AbstractDomain> ad){
+    const auto find = vars.find(v);
+    if(find != vars.end()){
+        if(ad.lessOrEqual(vars[v]))
+            return false;
+        vars[v] = vars[v].leastUpperBound(ad);
+
+    } else {
+        vars[v] = ad;
+    }
+    return true;
+}
+
+void State::leastUpperBound(State& other){
+    for(auto& [key,value] : vars){
+        put(key,value);
+    }
 }
 
 void VsaVisitor::visitPHINode(PHINode &I) {
@@ -21,6 +37,17 @@ void VsaVisitor::visitBinaryOperator(BinaryOperator &I) {
     if(true)
         this->pushInstUsers(I);
 
+}
+
+void VsaVisitor::visitUnaryInstruction(UnaryInstruction &I) {
+
+    if(true)
+        this->pushInstUsers(I);
+
+}
+
+void VsaVisitor::visitInstruction(Instruction &I){
+    errs() << I.getOpcodeName() << ": " << I.getValueID() << "\n";
 }
 
 void VsaVisitor::pushInstUsers(Instruction &I) {
--- src/vsa_visitor.h
+++ src/vsa_visitor.h
@@ -3,25 +3,67 @@
 
 #include "llvm/IR/InstVisitor.h"
 #include "llvm/IR/InstrTypes.h"
+#include "AbstractDomain.h"
 #include <queue>
+#include <unordered_map>
 
 using namespace llvm;
 
 namespace pcpo{
+
+class State{
+
+public:
+    State() = default;
+
+    bool put(Value& v, AbstractDomain& ad);
+    void leastUpperBound(State& other);
+
+private:
+    std::unordered_map<Value,AbstractDomain> vars;
+};
 
 class VsaVisitor : public InstVisitor<VsaVisitor,void> {
 
 public:
     VsaVisitor(std::queue<Instruction*>& q):worklist(q) { };
 
-    void visitInstruction(Instruction &I);
+    /// Specific Instruction type classes
+    /*void visitBranchInst(BranchInst &I);
+    void visitSwitchInst(SwitchInst &I);
+    void visitIndirectBrInst(IndirectBrInst &I);
+    void visitResumeInst(ResumeInst &I);
+    void visitICmpInst(ICmpInst &I);
+    void visitLoadInst(LoadInst &I);
+    void visitAtomicCmpXchgInst(AtomicCmpXchgInst &I);*/
     void visitPHINode(PHINode &I);
+    /*void visitTruncInst(TruncInst &I);
+    void visitZExtInst(ZExtInst &I);
+    void visitSExtInst(SExtInst &I);
+    void visitBitCastInst(BitCastInst &I);
+    void visitSelectInst(SelectInst &I);
+    void visitVAArgInst(VAArgInst &I);
+    void visitExtractElementInst(ExtractElementInst &I);
+    void visitExtractValueInst(ExtractValueInst &I);*/
+
+    /// Call and Invoke
+    //void visitCallInst(CallInst &I);
+    //void visitInvokeInst(InvokeInst &I);
+
+    /// if not specific get the whole class
+    //void visitCastInst(CastInst &I);
     void visitBinaryOperator(BinaryOperator& I);
+    //void visitCmpInst(CmpInst &I);
+    void visitUnaryInstruction(UnaryInstruction &I);
+
+    /// default
+    void visitInstruction(Instruction &I);
 
 private:
     void pushInstUsers(Instruction &I);
 
     std::queue<Instruction*>& worklist;
+    std::unordered_map<BasicBlock,State> programPoints;
 };
 
 }
