8080fdfff4ac41c3fd6609cbaa0cbd0de0eb734b
peterrum <peterrmuench@aol.com>
2018-06-09 22:24:45 +0200

    Rename test program

--- samples/while-1.c
+++ samples/while-1.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
+// test program:
+// simple loop
+
 int main(int argc, char const *argv[]) {
   int x = 0;
 
-  while(x < 17)
+  // at this point x={0}
+  while (x < 17)
     x++;
 
+  // at this point x={17}
   printf("%d\n", x);
 }
