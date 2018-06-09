b47de7946dabb6816696c72ccea50dd64acec27a
Michael Schwarz <michael.schwarz93@gmail.com>
2018-06-09 14:51:32 +0200

    Only add a basicblock to the worklist from current basic block if its branch condition is not \bottom. References #13

--- src/state.cpp
+++ src/state.cpp
@@ -7,22 +7,22 @@
 
 using namespace llvm;
 namespace pcpo {
-    
-State::State() : bottom(true){
+
+State::State() : conditionCacheUsed(false), bottom(true){
 }
 
 bool State::put(Value& v, std::shared_ptr<AbstractDomain> ad){
-    
+
     //assert(!bottom && "Visited although bottom!");
-    
+
     if(ad->lessOrEqual(*BoundedSet::create_bottom())){
         DEBUG_OUTPUT("State::put: set to bottom because of " << v.getName());
         ad->printOut();
         bottom = true;
         return true;
     }
-        
-    
+
+
     DEBUG_OUTPUT("State::put for " << v.getName());
     //ad->printOut();
     if(vars.find(&v) != vars.end()){
@@ -44,7 +44,7 @@ bool State::leastUpperBound(State& other){
     return change;
 }
 
-/// remove all <key,value> from this state where other does not have the key 
+/// remove all <key,value> from this state where other does not have the key
 /// \param other other state
 void State::prune(State& other){
     auto temp = vars;
@@ -53,27 +53,27 @@ void State::prune(State& other){
         if(other.vars.find(var.first)==other.vars.end()){
             temp.erase(var.first);
             DEBUG_OUTPUT("   and removes");
-            
+
         }
     }
     vars = temp;
 }
 
 shared_ptr<AbstractDomain> State::getAbstractValue(Value* v){
-    
+
     if(ConstantInt::classof(v)){
         auto temp = reinterpret_cast<ConstantInt*>(v);
         return shared_ptr<AbstractDomain>(new BoundedSet(temp->getValue()));
     }
-    
+
     auto find = vars.find(v);
     if(find != vars.end()){
         return find->second;
     }
-    
-    
+
+
     DEBUG_OUTPUT( "State::getAbstractValue " << v->getName() <<" : failed" );
-    
+
     return AD_TYPE::create_top();
 }
 
@@ -86,25 +86,25 @@ std::pair<Value *, std::shared_ptr < AbstractDomain>> State::get_branch_conditio
 }
 
 void State::applyCondition(BasicBlock* bb){
-    
+
     assert(!conditionCacheUsed && "ConditionCache has not been correctly unapplied last time!");
-    
+
     if(branchConditions.find(bb)!=branchConditions.end()){
         auto& branchCondition = branchConditions[bb];
         conditionCacheUsed = true;
-        
+
         auto value = branchCondition.first;
         conditionCache = std::pair<Value *, std::shared_ptr < AbstractDomain>>(
                 value, getAbstractValue(value));
-        
+
         vars[value] = branchCondition.second;
-    } 
+    }
 }
 
 void State::unApplyCondition(){
-    
+
     if(!conditionCacheUsed) return;
-    
+
     vars[conditionCache.first] = conditionCache.second;
     conditionCacheUsed = false;
 }
@@ -122,19 +122,19 @@ void State::print(){
         STD_OUTPUT("bottom");
         return;
     }
-    
+
     for(auto & var : vars){
         //std::cout << *var.second << std::endl;
         STD_OUTPUT(var.first->getName() << " -> " << *var.second);
         //var.second->printOut();
     }
-    
+
 }
 
 void State::putBranchConditions(BasicBlock* bb, Value * val, std::shared_ptr < AbstractDomain> ad){
 
     branchConditions[bb] = std::pair<Value *, std::shared_ptr < AbstractDomain>>(val, ad);
-    
+
 }
 
 void State::transferBranchConditions(State& other) {
