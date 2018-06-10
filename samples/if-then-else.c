56269aae56671f2587d0b2e4083e63c89900e81b
Michael Schwarz <michael.schwarz93@gmail.com>
2018-06-10 22:24:58 +0200

    Add example that shows problem with LUB References #20

---
+++ samples/if-then-else.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+// test program:
+// simple loop
+
+int main(int argc, char const *argv[]) {
+  int x = 42;
+  x += 7;
+  int y = rand();
+
+  if (x < y){
+    int z = 42;
+    printf("%d\n", z);
+  }
+
+  printf("%d\n", x);
+  printf("%d\n", y);
+}
