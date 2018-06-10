c09896f26ce6ca5b99d5e8c3e243379e11df346e
peterrum <peterrmuench@aol.com>
2018-06-10 16:59:34 +0200

    Add test program with while loop exeeding max nr. of bounded set size

---
+++ samples/while-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// test program:
+// simple loop
+
+int main(int argc, char const *argv[]) {
+  int x = 0;
+
+  // at this point x={0}
+  while (x < 100)
+    x++;
+
+  // at this point x={17}
+  printf("%d\n", x);
+}
