90c53869ff05f236b9efb92662ceaf3c79d70010
alex <a.roschlaub@tum.de>
2018-06-12 14:59:52 +0200

    Add testFile ops.c for testing binaryOperations

---
+++ samples/ops.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+
+int a = 5;
+int b = -2;
+
+unsigned aU = 5;
+unsigned bU = 2;
+
+int cC = a+1;
+int cV = a+b;
+
+int dC = a-1;
+int dV = a-b;
+
+int eC = a*2;
+int eV = a*b;
+
+unsigned fuC = aU/5;
+int fsC = a/5;
+unsigned fuV = aU/bU;
+int fsV = a/b;
+
+unsigned guC = aU%5;
+int gsC = a%5;
+unsigned guV = aU/bU;
+int gsV = a/b;
+
+int hC = a << 1;
+int hV = a << bU;
+
+unsigned iuC = aU >> 1;
+int isC = a >> 1;
+unsigned iuV = aU >> bU;
+int isV = a >> bU;
+
+int jC = a & 1;
+int jV = a & bU;
+
+int kC = a | 1;
+int kV = a | bU;
+
+int lC = a^1;
+int lV = a^bU;
+}
