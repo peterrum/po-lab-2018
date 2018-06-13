df3698dea1cab734e435afc1c4ca4d43dc3b7045
Julian Erhard <julian.erhard@tum.de>
2018-06-13 09:27:46 +0200

    Add include for vsa_result_value to fix compilation

--- src/api/vsa_result_value.cpp
+++ src/api/vsa_result_value.cpp
@@ -1,6 +1,7 @@
 #include <llvm/Analysis/LazyValueInfo.h>
 
 #include "vsa_result_value.h"
+#include "llvm/IR/Constants.h"
 using namespace llvm;
 
 namespace pcpo {
@@ -12,13 +13,13 @@ LazyValueInfo::Tristate VsaResultValue::testIf(CmpInst::Predicate predicate,
   if (ConstantInt::classof(C)) {
     // create abstract domain for constant
     auto temp = AD_TYPE(reinterpret_cast<ConstantInt *>(C)->getValue());
-    
+
     // perform comparison
     auto result = abstractValue->icmp(predicate,
                                       C->getType()->getIntegerBitWidth(), temp);
 
     // temp. bools
-    bool b_t = !result.first->isBottom(); // predicate might be true
+    bool b_t = !result.first->isBottom();  // predicate might be true
     bool b_f = !result.second->isBottom(); // predicate might not be true
 
     if (b_t && b_f) // predicate might be true or not
