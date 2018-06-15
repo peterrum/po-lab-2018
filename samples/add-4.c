a19179ca43a9329a10961c41cc50ffa391dbeb60
peterrum <peterrmuench@aol.com>
2018-06-15 13:25:03 +0200

    Add simple example for less or equal

---
+++ samples/add-4.c
@@ -0,0 +1,13 @@
+int test() {
+    int a = 1; a++; // a={2}
+    int b = 2; b++; // b={3}
+    int c = 3; c++; // c={4}
+    
+    if(c <= a){     // bottom     
+        c += 1;     // 
+        a = c;      // 
+    }
+    
+                    // a={12}
+    return a + b;   // ret={5}
+}
