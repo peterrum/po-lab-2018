7ea609b2d2f22890fb6d9cb25ae6346260a1cf5d
Michael Schwarz <michael.schwarz93@gmail.com>
2018-06-17 01:45:55 +0200

    Add example. References #52

---
+++ samples/switch-two-labels.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+// test program:
+// simple loop
+
+int main(int argc, char const *argv[]) {
+  int x = rand();
+  int y = 0;
+  y++;
+
+  switch (x) {
+    case 3:
+    case 8:
+      y = x;
+      break;
+    case 5:
+      y = x<<1;
+  }
+
+  int z = y+7;
+  printf("%d\n", z);
+}
