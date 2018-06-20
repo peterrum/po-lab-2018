0cef5d33701ddfb5e0e22ec6297d737f02d1839c
alex <a.roschlaub@tum.de>
2018-06-20 15:43:46 +0200

    example with other stride than only stride 1 (references #57)

--- samples/example.c
+++ samples/example.c
@@ -14,25 +14,25 @@
 
 int main(int argc, char const *argv[]) {
     unsigned a = 0;
-    unsigned b = 6;
+    unsigned b = 12;
 
     while (a < b) {
-        a++;
-        b--;
+        a+=4;
+        b-=2;
     }
 
     unsigned c = rand();
-    if(a>3 && b<3){
+    if(a>6 && b<6){
         switch (a) {
-            case 3:
+            case 6:
                 b = 99;
                 break;
-            case 6:
-            case 7:
+            case 12:
+            case 13:
                 b = a*2;
                 break;
             default:
-                c = c%13;
+                c = c%18;
         }
     } else {
         a = 88;
